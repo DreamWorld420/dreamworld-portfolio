41949a50de1ed7e654edb075457cd4a99856adf5
DreamWorld420 <dreamworld420@protonmail.com>
2025-08-03 10:50:27 +0330

    feat: checkpoint

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,81 @@
 import Image from "next/image";
-import styles from "./page.module.css";
+import Link from "next/link";
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+	return (
+		<div className="container mx-auto grid grid-cols-1 md:grid-cols-12 py-20">
+			<div className="md:col-span-3 flex flex-col gap-4">
+				<div className="flex items-center gap-4 ">
+					<Image
+						alt="profile"
+						src={"/personal-profile.jpeg"}
+						width={100}
+						height={100}
+						className="rounded-full border border-solid border-neutral-100"
+						quality={100}
+					/>
+					<div>
+						<h1 className="text-xl font-bold">Kasra Bozorgmehr</h1>
+						<p className="text-sm text-neutral-400">Front-End Developer</p>
+						<Link
+							href="mailto:dreamworld420@protonmail.com"
+							className="text-blue-500 hover:underline"
+						>
+							Let's Talk
+						</Link>
+					</div>
+				</div>
+				<p className="text-neutral-300">
+					Welcome to my space—where modern, responsive, and creative web
+					experiences come to life.
+				</p>
+				<div className="flex gap-2 items-center">
+					<div className="h-[1px] bg-neutral-100 grow"></div>
+					<span>~</span>
+					<div className="h-[1px] bg-neutral-100 grow"></div>
+				</div>
+				<div className="flex flex-col gap-2">
+					<h2 className="text-lg font-semibold">Connect with me</h2>
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+					<div className="flex items-center gap-2">
+						<i className="bi bi-linkedin"></i>
+						<Link
+							href="https://www.linkedin.com/in/kasra-bozorgmehr/"
+							className="hover:underline"
+						>
+							Linkedin
+						</Link>
+					</div>
+					<div className="flex items-center gap-2">
+						<i className="bi bi-github"></i>
+						<Link
+							href="https://github.com/DreamWorld420"
+							className="hover:underline"
+						>
+							Github
+						</Link>
+					</div>
+					<div className="flex items-center gap-2">
+						<i className="bi bi-stack-overflow"></i>
+						<Link
+							href="https://stackoverflow.com/users/11162007/kasra?tab=profile"
+							className="hover:underline"
+						>
+							Stackoverflow
+						</Link>
+					</div>
+					<div className="flex items-center gap-2">
+						<i className="bi bi-envelope-fill" />
+						<Link
+							href="mailto:dreamworld420@protonmail.com"
+							className="hover:underline"
+						>
+							dreamworld420@protonmail.com
+						</Link>
+					</div>
+				</div>
+			</div>
+			<div className="md:col-span-9"></div>
+		</div>
+	);
 }
