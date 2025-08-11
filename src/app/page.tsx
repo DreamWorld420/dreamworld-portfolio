afb2ee15648949dbb7dece77ae664c57207bb5df
DreamWorld420 <dreamworld420@protonmail.com>
2025-08-11 21:39:19 +0330

    feat: changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -123,6 +123,42 @@ export default async function Home() {
 				className="hide-scrollbar flex h-fit flex-col gap-8 overflow-auto pb-10 md:col-span-8 md:max-h-[calc(100vh_-_80px)] md:pb-20 md:pt-0"
 			>
 				<AboutMe />
+				{blogFeed && blogFeed?.items?.length && (
+					<section className="flex flex-col gap-2">
+						<h2 className="mb-2 border-b border-solid border-neutral-100 font-serif text-3xl font-bold">
+							Blog Posts
+						</h2>
+						<div className="flex flex-col gap-2">
+							{blogFeed.items.map(({ title, pubDate, link, categories }) => (
+								<Link key={title} href={link} target="blank" className="group">
+									<div className="flex flex-col gap-1">
+										<p className="text-sm text-neutral-300">
+											{formatDate(new Date(pubDate))}
+										</p>
+										<h3 className="font-serif text-xl font-semibold">
+											{title}{" "}
+											<i className="bi bi-arrow-up-right text-base transition-all group-hover:text-blue-500"></i>
+										</h3>
+										{categories.length && (
+											<div className="mt-1 flex flex-wrap gap-2">
+												{categories.map((item) => {
+													return (
+														<div
+															className="text-nowrap rounded-3xl border border-solid border-blue-200 bg-blue-200 bg-opacity-15 px-2 text-xs font-bold text-blue-200 md:text-sm"
+															key={item}
+														>
+															{item}
+														</div>
+													);
+												})}
+											</div>
+										)}
+									</div>
+								</Link>
+							))}
+						</div>
+					</section>
+				)}
 				<section className="flex flex-col gap-2">
 					<h2 className="mb-2 border-b border-solid border-neutral-100 font-serif text-3xl font-bold">
 						Projects
@@ -194,42 +230,6 @@ export default async function Home() {
 						/>
 					</div>
 				</section>
-				{blogFeed && blogFeed?.items?.length && (
-					<section className="flex flex-col gap-2">
-						<h2 className="mb-2 border-b border-solid border-neutral-100 font-serif text-3xl font-bold">
-							Blog
-						</h2>
-						<div className="flex flex-col gap-2">
-							{blogFeed.items.map(({ title, pubDate, link, categories }) => (
-								<Link key={title} href={link} target="blank" className="group">
-									<div className="flex flex-col gap-1">
-										<p className="text-sm text-neutral-300">
-											{formatDate(new Date(pubDate))}
-										</p>
-										<h3 className="font-serif text-xl font-semibold">
-											{title}{" "}
-											<i className="bi bi-arrow-up-right text-base transition-all group-hover:text-blue-500"></i>
-										</h3>
-										{categories.length && (
-											<div className="mt-1 flex flex-wrap gap-2">
-												{categories.map((item) => {
-													return (
-														<div
-															className="text-nowrap rounded-3xl border border-solid border-blue-200 bg-blue-200 bg-opacity-15 px-2 text-xs font-bold text-blue-200 md:text-sm"
-															key={item}
-														>
-															{item}
-														</div>
-													);
-												})}
-											</div>
-										)}
-									</div>
-								</Link>
-							))}
-						</div>
-					</section>
-				)}
 			</div>
 		</div>
 	);
